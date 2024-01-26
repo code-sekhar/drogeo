e76118b475d3ebc4da31f80fad50a08bddc55d70
sekhar <sekharchaudhary5@gmail.com>
2024-01-26 23:19:00 +0530

    product

--- js/script.js
+++ js/script.js
@@ -50,4 +50,34 @@ for (const locale of locales) {
   }
 }
 
-// slider
+
+//product_cncrement_decrement 
+let counter = 1;
+
+function increment() {
+  counter++;
+}
+
+function decrement() {
+  counter--;
+}
+
+function get() {
+  return counter;
+}
+
+const inc = document.getElementById("increment");
+const input = document.getElementById("input");
+const dec = document.getElementById("decrement");
+
+inc.addEventListener("click", () => {
+  increment();
+  input.value = get();
+});
+
+dec.addEventListener("click", () => {
+  if (input.value > 0) {
+    decrement();
+  }
+  input.value = get();
+});
